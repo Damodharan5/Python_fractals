0d6c625abe0be9137998562008c5c04727dbbd7c
Damodharan5 <damolovestiger@ymail.com>
2016-03-28 19:09:03 -0500

    Update Triangle.py

--- Fractals_1/Triangle.py
+++ Fractals_1/Triangle.py
@@ -1,1 +1,26 @@
+#Coding Starts
+
+```python
 # The Sierpinski Triangle
+
+import turtle
+
+wn = turtle.Screen()
+
+alex = turtle.Turtle()
+
+def line(x1,y1,x2,y2):
+	alex.penup()
+	alex.goto(x1,y1)
+	alex.pendown()
+	alex.goto(x2,y2)
+	
+def eqtr(x1,y1,x2,y2,x3,y3):
+	line(x1,y1,x2,y2)
+	line(x3,y3,x2,y2)
+	line(x1,y1,x3,y3)   
+
+
+
+```
+
