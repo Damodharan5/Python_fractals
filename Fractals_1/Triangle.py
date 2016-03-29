a02dfba72c1adf9c01837a5cd20308250a324646
Damodharan5 <damolovestiger@ymail.com>
2016-03-28 19:13:52 -0500

    Update Triangle.py
    eqtr(len/2,-1.732*len/6,-len/2,-1.732*len/6,0,len/1.732) - Function call to draw equilateral triangle.
    
    It needs three vertices of triangle. To calculate three vertices with known length and centroid as (0,0), we need to use the formula passed as arguments above.

--- Fractals_1/Triangle.py
+++ Fractals_1/Triangle.py
@@ -1,6 +1,4 @@
 #Coding Starts
-
-```python
 # The Sierpinski Triangle
 
 import turtle
@@ -20,7 +18,16 @@ def eqtr(x1,y1,x2,y2,x3,y3):
 	line(x3,y3,x2,y2)
 	line(x1,y1,x3,y3)   
 
-
-
-```
-
+def treee(x1,y1,x2,y2,x3,y3,len,min):
+	eqtr((x1+x2)/2,(y1+y2)/2,(x2+x3)/2,(y2+y3)/2,(x3+x1)/2,(y3+y1)/2)
+	if(len >= min):
+		treee(x1,y1,(x1+x2)/2,(y1+y2)/2,(x3+x1)/2,(y3+y1)/2,len/2,min)
+		treee(x2,y2,(x2+x3)/2,(y2+y3)/2,(x1+x2)/2,(y1+y2)/2,len/2,min)
+		treee(x3,y3,(x2+x3)/2,(y2+y3)/2,(x3+x1)/2,(y3+y1)/2,len/2,min)
+		
+if __name__ == "__main__":
+	len=input("Enter the side: ")
+	eqtr(len/2,-1.732*len/6,-len/2,-1.732*len/6,0,len/1.732)
+	treee(len/2,-1.732*len/6,-len/2,-1.732*len/6,0,len/1.732,len,input("Enter the min size: "))   # The Sierpinski Triangle
+	alex.hideturtle()
+	wn.exitonclick()
